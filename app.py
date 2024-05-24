c2b23d49c021167cad235b5bbfe4200092634bdb
sethuraman17 <116188101+sethuraman17@users.noreply.github.com>
2024-05-25 02:22:36 +0530

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -2,24 +2,23 @@ import http.server
 import socketserver
 from time import sleep
 import cv2
-import threading 
+import threading
 
 PORT = 2020
 
 pageData = "<!DOCTYPE>" + \
-            "<html>" + \
-            "  <head>" + \
-            "    <title>HttpListener Example</title>" + \
-            "  </head>" + \
-            "  <body>" + \
-            "<img id=\"image\"/>"+ \
-            " <script type=\"text/javascript\">var image = document.getElementById('image');function refresh() {image.src = \"/image?\" + new Date().getTime();image.onload= function(){setTimeout(refresh, 30);}}refresh();</script>   "+ \
-            "  </body>" + \
-            "</html>"
+           "<html>" + \
+           "  <head>" + \
+           "    <title>HttpListener Example</title>" + \
+           "  </head>" + \
+           "  <body>" + \
+           "<img id=\"image\"/>" + \
+           " <script type=\"text/javascript\">var image = document.getElementById('image');function refresh() {image.src = \"/image?\" + new Date().getTime();image.onload= function(){setTimeout(refresh, 30);}}refresh();</script>   " + \
+           "  </body>" + \
+           "</html>"
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        
         if self.path == '/':
             self.send_response(200)
             self.send_header("Content-type", "text/html")
@@ -29,32 +28,30 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "image/jpeg")
             self.end_headers()
-
-            # ret, frame = cap.read()
-            # _, jpg = cv2.imencode(".jpg", frame)
-
             self.wfile.write(jpg)
         else:
             self.send_response(404)
-        
+
 # continuously get frames from webcam
-cap = cv2.VideoCapture(0)  
+cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 _, jpg = cv2.imencode(".jpg", frame)
-  
-class FrameThread (threading.Thread):
-    def _init_(self):
-        threading.Thread._init_(self)
+
+class FrameThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
         self.isRunning = True
 
     def run(self):
         global jpg, cap
-
         while self.isRunning:
             ret, frame = cap.read()
-            _, jpg = cv2.imencode(".jpg", frame)
+            if ret:
+                _, jpg = cv2.imencode(".jpg", frame)
+                cv2.imshow("Images", frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
             sleep(0.03)
-
         print("Quit thread")
 
 frame_thread = FrameThread()
@@ -65,7 +62,7 @@ with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("Serving at port ", PORT)
     try:
         httpd.serve_forever()
-    except:
+    except KeyboardInterrupt:
         pass
 
 # quit the app
@@ -73,3 +70,4 @@ print('Server is stopped')
 frame_thread.isRunning = False
 frame_thread.join()
 cap.release()
+cv2.destroyAllWindows()
